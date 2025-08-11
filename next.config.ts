7993cae885496185b677a40f8294ad9cc92358b2
Christian Galeone <ch.galeone@reply.it>
2025-08-11 23:42:56 +0200

    No image optimization

--- next.config.ts
+++ next.config.ts
@@ -22,8 +22,7 @@ const nextConfig: NextConfig = {
    * @see https://nextjs.org/docs/app/api-reference/components/image#unoptimized
    */
   images: {
-    loader: 'custom',
-    loaderFile: './src/utils/imageLoader.ts',
+    unoptimized: true,
   },
 };
 
